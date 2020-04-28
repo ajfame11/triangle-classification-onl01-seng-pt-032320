623e56676a1f19346b157d7cff2fc6aa4b94dae4
aurel fontaine <ajfame11@gmail.com>
2020-04-28 20:26:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,5 @@
 class Triangle
   
-
-  def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
-      raise TriangleError
-    end
-  end
-end
-
-class TriangleError < StandardError
+  
 
 end
