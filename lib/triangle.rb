7563ad0e57f6013bcff5cef35dc123f128f0de3b
aurel fontaine <ajfame11@gmail.com>
2020-04-28 19:58:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,5 @@
 class Triangle
+  
   def initialize(side_1, side_2, side_3)
     @triangle_sides = []
     @triangle_sides << side_1
@@ -6,18 +7,4 @@ class Triangle
     @triangle_sides << side_3
   end
   
-  def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
-      raise TriangleError
-    end
-  end
-end
 end
